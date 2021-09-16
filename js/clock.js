789c2c401c5856ee8ebdc20df37d1a209454efa8
seonjoo <seonjoo2030@hanmil.net>
2021-09-16 22:56:37 +0900

    Date object 사용

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,13 @@
 const clock = document.querySelector("h2#clock");
 
- function sayHallo()
+function getClock()
 {
-    console.log("hello");
+    const data = new Date(); // Date object
+    const hours = String(data.getHours()).padStart(2, "0");
+    const minutes = String(data.getMinutes()).padStart(2, "0");
+    const seconds = String(data.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-//setInterval(sayHallo, 5000); // 함수 명, 밀리 초 - interval 개념 
-setTimeout(sayHallo, 5000);
+getClock(); // 새로 고침 시, html에서 표시한 초기 값인 00:00이 출력되지 않도록 한번 수행.
+setInterval(getClock, 1000);
